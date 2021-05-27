9139a8e242528333334ee673b340f8b35f67904b
UMAMAHESHWARIG <umamaheshwari.g@sonata-software.com>
2021-05-27 11:30:43 +0530

    pushing Form
    state managment

--- matter/src/Matter.js
+++ matter/src/Matter.js
@@ -17,7 +17,92 @@ initializeIcons();
 
 
   
+export default class Matter extends Component {
+    userData;
 
+    constructor(props) {
+        super(props);
+
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePhone = this.onChangePhone.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            name: '',
+            email: '',
+            phone: ''
+        }
+    }
+
+    // Form Values
+    onChangeName(e) {
+        this.setState({ name: e.target.value })
+    }
+
+    onChangeEmail(e) {
+        this.setState({ email: e.target.value })
+    }
+
+    onChangePhone(e) {
+        this.setState({ phone: e.target.value })
+    }
+
+
+    // React Life Cycle
+    componentDidMount() {
+        this.userData = JSON.parse(localStorage.getItem('user'));
+
+        if (localStorage.getItem('user')) {
+            this.setState({
+                name: this.userData.name,
+                email: this.userData.email,
+                phone: this.userData.phone
+            })
+        } else {
+            this.setState({
+                name: '',
+                email: '',
+                phone: ''
+            })
+        }
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('user', JSON.stringify(nextState));
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        console.log(this.state.props)
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" className="form-control" value={this.state.name} onChange={this.onChangeName} />
+                    </div>
+                    <br></br>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" className="form-control" value={this.state.email} onChange={this.onChangeEmail} />
+                    </div>
+                    <br></br>
+                    <div className="form-group">
+                        <label>Phone</label>
+                        <input type="tel" className="form-control" value={this.state.phone} onChange={this.onChangePhone} />
+                    </div>
+                    <br></br>
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
 
 
 
@@ -52,21 +137,21 @@ initializeIcons();
 // };
 
 
-const Matter = () => {
+// const Matter = () => {
 
  
-    return ( 
-        <div>
+//     return ( 
+//         <div>
         
-           <div className="wrapper">
-           <div><DialogModelessExample/></div>
-           <div style={{width:"700px"}}><SearchBox placeholder="Search" / ></div>
-            </div>
-             <DetailsListDocumentsExample/>
+//            <div className="wrapper">
+//            <div><DialogModelessExample/></div>
+//            <div style={{width:"700px"}}><SearchBox placeholder="Search" / ></div>
+//             </div>
+//              <DetailsListDocumentsExample/>
              
-        </div>
-        );
-    }
+//         </div>
+//         );
+//     }
 
 
-export default Matter;
+//export default Matter;
